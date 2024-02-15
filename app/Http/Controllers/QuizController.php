854617f856ffa71c9fbe97b298d440f30e3a3519
Shuhei Hashimoto <159013664+HashimotoShuhei-z@users.noreply.github.com>
2024-02-15 17:02:04 +0900

    追加処理と削除処理のコントローラを追加

---
+++ app/Http/Controllers/QuizController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuizController extends Controller
+{
+    //
+    public function store(Request $request)
+    {
+        //入力内容を検証
+
+        //データを保存
+
+        return view('quizzes.index');
+    }
+
+    public function destroy($id)
+    {
+        return json_encode(['message' => 'ID:'.$id.' が削除されるIDです(仮の出力)']);
+    }
+}
